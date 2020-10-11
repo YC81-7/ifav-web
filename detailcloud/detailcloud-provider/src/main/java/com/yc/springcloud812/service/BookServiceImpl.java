d67606cafb7706a9ca9ede64b001307497c23859
tph81 <2378868011@qq.com>
2020-10-11 19:32:16 +0800

    Revert "Initial commit"
    This reverts commit d00c71f5f1580beaacdd06e79fd3351a751adf19.

---
+++ detailcloud/detailcloud-provider/src/main/java/com/yc/springcloud812/service/BookServiceImpl.java
@@ -0,0 +1,32 @@
+package com.yc.springcloud812.service;
+
+
+
+import com.yc.springcloud812.bean.Book;
+import com.yc.springcloud812.dao.BookMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class BookServiceImpl implements BookService{
+
+    @Autowired(required = false)
+    private BookMapper bookMapper;
+
+    @Override
+    public Book getBook(Integer id) {
+        //selectByPrimaryKey根据主键id查
+        return bookMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        //查询所有
+        return bookMapper.selectAll();
+    }
+
+
+}
